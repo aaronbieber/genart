423a06d0ef10b148ae18d951ea881e324ef99d2d
Aaron Bieber <aaron@aaronbieber.com>
2021-12-02 06:32:45 -0500

    Move mapColor to utils
    mapColor provably works better than lerpColor to create smooth
    transitions between any two colors.

--- static/utils.js
+++ static/utils.js
@@ -8,3 +8,11 @@ function makeCanvas(maxWidth = 1000, maxHeight = 1000) {
 
   return canvas;
 }
+
+function mapColor(c1, c2, pct) {
+  colorMode(RGB);
+  return color(
+    map(pct, 0, 1, red(c1), red(c2)),
+    map(pct, 0, 1, green(c1), green(c2)),
+    map(pct, 0, 1, blue(c1), blue(c2)));
+}
