ef78407e49a2655ebdc3361acfacf83edee44c0b
Aaron Bieber <aaron@aaronbieber.com>
2021-12-05 17:05:59 -0500

    Update Loops

--- content/sketches/loops.js
+++ content/sketches/loops.js
@@ -1,4 +1,4 @@
-var center, point, prevPoint, palettes, palette;
+var center, loop, prevLoop, palettes, palette;
 var orbitAngleInc = 0;
 var loopAngleInc = 0;
 var noiseInc = 0;
@@ -15,22 +15,22 @@ function setup() {
   stroke(palette[4]);
 
   center = createVector(width/2, height/2);
-  point = createVector(width/2, height/2);
-  prevPoint = 0;
+  loop = createVector(width/2, height/2);
+  prevLoop = 0;
   noiseScale = shortEdge() / 3;
   frameRate(999);
 }
 
-function getColor(val) {
+function getColor(val, c1, c2) {
   let max = 50;
   let pos = val % max;
   if (pos < max/2) {
     // toward color 2
-    return mapColor(palette[1], palette[3],
+    return mapColor(c1, c2,
                     map(pos, 0, max/2, 0, 1));
   } else {
     // toward color 1
-    return mapColor(palette[3], palette[1],
+    return mapColor(c2, c1,
                     map(pos, max/2, max, 0, 1));
   }
 }
@@ -40,26 +40,26 @@ function draw() {
   let orbitAngle = orbitAngleInc % TWO_PI;
   let noiseVal = noise(noiseInc) * noiseScale;
 
-  strokeWeight(noiseVal/40);
-  stroke(getColor(loopAngleInc));
+  strokeWeight(map(sin(noiseInc % TWO_PI), 1, 0, 1, 50, true));
 
   center.set(
     (sin(orbitAngle) * noiseVal) + (width/2),
     (cos(orbitAngle) * noiseVal) + (height/2));
 
-  point.set(
+  loop.set(
     (cos(loopAngle) * noiseVal/1.5) + center.x,
     (sin(loopAngle) * noiseVal/1.5) + center.y
   );
 
-  if (prevPoint !== 0) {
-    line(prevPoint.x, prevPoint.y, point.x, point.y);
+  if (prevLoop !== 0) {
+    stroke(getColor(loopAngleInc, palette[1], palette[3]));
+    line(prevLoop.x, prevLoop.y, loop.x, loop.y);
   } else {
-    prevPoint = point.copy();
+    prevLoop = loop.copy();
   }
 
   orbitAngleInc += 0.02;
   loopAngleInc += 0.2;
   noiseInc += 0.01;
-  prevPoint.set(point);
+  prevLoop.set(loop);
 }
